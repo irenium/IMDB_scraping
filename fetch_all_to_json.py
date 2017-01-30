330f89deb5ca7972724c05c7812c23f2c5990356
Irene <irenechen17@gmail.com>
2017-01-30 10:25:18 -0800

    working code to fetch all data and save to json file

---
+++ fetch_all_to_json.py
@@ -0,0 +1,35 @@
+import json
+#import argparse
+import csv
+#import operator
+#import codecs
+#import cStringIO
+#import urllib2
+import sys
+import movie_scrapers
+
+# Make a list of movie dictionaries:
+
+movie_dicts = []
+movie_urls  = []
+for row in csv.reader(open("imdb_url_list.csv", "rb")):
+    movie_urls.append(row[0])
+current_idx = 0
+m = len(movie_urls)
+
+for url in movie_urls:
+    movie_object = movie_scrapers.Movie(url)
+    movie_dicts.append(movie_object.to_dict())
+    
+    if (current_idx % 10) == 0:
+        progress = 100.0 * current_idx / m
+        sys.stdout.write('\r progress {} / {} ({:6.2f}%)'.format(current_idx, m, progress))
+        sys.stdout.flush()
+    current_idx += 1
+        
+print len(movie_dicts)    
+
+# Write list of movie dictionaries to a json file:
+
+with open('movies_testrun.json','wb') as outfile:
+    json.dump(movie_dicts, outfile, indent = 2, separators = (',',': '))
