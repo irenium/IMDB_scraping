5ae73f99386cc4a2c1d154f5966760fc405ebcd8
Irene <irenechen17@gmail.com>
2017-02-16 15:47:17 -0800

    updated html, organized plotting code

--- plot_movies.py
+++ plot_movies.py
@@ -1,12 +1,23 @@
 import csv
+import jinja2
 import json
 import numpy as np
 import pandas as pd
+
+from bokeh.core.templates import JS_RESOURCES
+from bokeh.embed import components
+from bokeh.layouts import column
+from bokeh.models import (
+    ColumnDataSource, Plot, Circle, Range1d,
+    LinearAxis, HoverTool, Text, HoverTool,
+    SingleIntervalTicker, Slider, CustomJS)
 from bokeh.palettes import Spectral7
+from bokeh.plotting import figure, ColumnDataSource
+from bokeh.resources import CDN
 
 
 def process_data_teams():
-    """Process movie data and return organized dataframes"""
+    """Process movie teams and return organized dataframes"""
 
     with open('movies_6feb2017.json','rb') as infile:
         movie_dicts = json.load(infile)
@@ -218,3 +229,139 @@ def get_team_members(credits_by_teams, teamname):
     (e.g., "Writing Credits" team)"""
 
     return [name for name, teamnames in credits_by_teams if teamname in teamnames]
+
+
+def make_interactive_plot():
+    """Copied from bokeh tutorials, and modified to make movie interactive plot"""
+
+    x_axis_df, y_axis_df, bubble_df, colors_df, labels_df, years, teams = process_data_teams()
+    
+    sources = {}
+
+    for year in years:
+        team_color = colors_df[year]
+        team_color.name = 'team_color'
+        
+        x_variable = x_axis_df[year]
+        x_variable.name = 'x_variable'
+        y_variable = y_axis_df[year]
+        y_variable.name = 'y_variable'
+        bubble_variable = bubble_df[year]
+        bubble_variable.name = 'bubble_variable'
+        labels = labels_df[year]
+        labels.name = 'labels'
+        new_df = pd.concat([x_variable, y_variable, bubble_variable, team_color, labels], axis=1)
+        sources['_' + str(year)] = ColumnDataSource(new_df)
+
+    dictionary_of_sources = dict(zip([x for x in years], ['_%s' % x for x in years]))
+    js_source_array = str(dictionary_of_sources).replace("'", "")
+
+    xdr = Range1d(1, 10)
+    ydr = Range1d(-50, 1200)
+    plot = Plot(
+        x_range=xdr,
+        y_range=ydr,
+        plot_width=800,
+        plot_height=400,
+        outline_line_color=None,
+        toolbar_location=None,
+        sizing_mode="scale_width",
+    )
+    AXIS_FORMATS = dict(
+        minor_tick_in=None,
+        minor_tick_out=None,
+        major_tick_in=None,
+        major_label_text_font_size="12pt",
+        major_label_text_font_style="normal",
+        axis_label_text_font_size="12pt",
+
+        axis_line_color='#AAAAAA',
+        major_tick_line_color='#AAAAAA',
+        major_label_text_color='#666666',
+
+        major_tick_line_cap="round",
+        axis_line_cap="round",
+        axis_line_width=1,
+        major_tick_line_width=1,
+    )
+
+    xaxis = LinearAxis(ticker=SingleIntervalTicker(interval=1), axis_label="IMDb Rating", **AXIS_FORMATS)
+    yaxis = LinearAxis(ticker=SingleIntervalTicker(interval=100), axis_label="Team Size", **AXIS_FORMATS)
+    plot.add_layout(xaxis, 'below')
+    plot.add_layout(yaxis, 'left')
+
+    # ### Add the background year text
+    # We add this first so it is below all the other glyphs
+    text_source = ColumnDataSource({'year': ['%s' % years[0]]})
+    text = Text(x=2, y=35, text='year', text_font_size='150pt', text_color='#EEEEEE')
+    #plot.add_glyph(text_source, text)
+
+    # Add the circle
+    renderer_source = sources['_%s' % years[0]]
+    circle_glyph = Circle(
+        x='x_variable', y='y_variable', size='bubble_variable',
+        fill_color='team_color', fill_alpha=0.8,
+        line_color='#7c7e71', line_width=0.5, line_alpha=0.5)
+    circle_renderer = plot.add_glyph(renderer_source, circle_glyph)
+
+    # Add the hover (only against the circle and not other plot elements)
+    tooltips = "@labels"
+    plot.add_tools(HoverTool(tooltips=tooltips, renderers=[circle_renderer]))
+
+    # Add the legend
+    text_x = 2
+    text_y = 1150
+    for i, team in enumerate(teams):
+        plot.add_glyph(Text(x=text_x, y=text_y, text=[team], text_font_size='10pt', text_color='#666666'))
+        plot.add_glyph(Circle(x=text_x - 0.1, y=text_y + 25, fill_color=Spectral7[i], size=10, line_color=None, fill_alpha=0.8))
+        text_y = text_y - 50
+
+    # Add the slider
+    code = """
+        var year = slider.get('value'),
+            sources = %s,
+            new_source_data = sources[year].get('data');
+        renderer_source.set('data', new_source_data);
+        text_source.set('data', {'year': [String(year)]});
+    """ % js_source_array
+
+    callback = CustomJS(args=sources, code=code)
+    slider = Slider(start=years[0], end=years[-1], value=1, step=1, title="Year", callback=callback, name='testy')
+    callback.args["renderer_source"] = renderer_source
+    callback.args["slider"] = slider
+    callback.args["text_source"] = text_source
+
+    # Lay it out
+    return column(plot, slider)
+
+HTML_TEMPLATE ="""
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>{{ title if title else "Bokeh Plot" }}</title>
+        <link href="assets/gapminder_styles.css" type="text/css" rel="stylesheet" />
+    </head>
+    <body>
+        {{ plot_div|indent(8) }}
+        {{ bokeh_js|indent(8) }}
+        {{ plot_script|indent(8) }}
+    </body>
+</html>
+"""
+
+
+def write_plot_html():
+    """Saves interactive plot as html (copied from bokeh tutorials)"""
+
+    layout = make_interactive_plot()
+    template = jinja2.Template(HTML_TEMPLATE)
+    script, div = components(layout)
+    html = template.render(
+        title="Resource Allocation in Animated Movies",
+        bokeh_js=JS_RESOURCES.render(js_raw=CDN.js_raw, js_files=CDN.js_files),
+        plot_script=script,
+        plot_div=div,
+    )
+    with open('interactive_plot_for_web2.html', 'wb') as outfile:
+        outfile.write(html)
